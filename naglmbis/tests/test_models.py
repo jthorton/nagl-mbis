865090821cbe3f592d35ab99d49f4648d494c23b
Josh Horton <Josh.Horton@newcastle.ac.uk>
2022-12-20 12:17:06 +0000

    add an option to change the activation functions for hyperparam tuning (#8)

--- naglmbis/tests/test_models.py
+++ naglmbis/tests/test_models.py
@@ -1,8 +1,6 @@
-import pytest
 import torch
 
-from naglmbis.models import load_charge_model, load_volume_model
-from naglmbis.models.models import EspalomaModel
+from naglmbis.models import MBISGraphModelV1, load_charge_model, load_volume_model
 
 
 def test_charge_model_v1(methanol):
@@ -27,3 +25,24 @@ def test_volume_model_v1(methanol):
     ].detach()
     ref = torch.Tensor([[29.6985], [25.3335], [3.0224], [3.0224], [3.0224], [1.0341]])
     assert torch.allclose(volumes, ref, atol=1e-4)
+
+
+def test_activation():
+    """
+    Test building a model with a new activation function
+    """
+    model = MBISGraphModelV1(
+        n_gcn_hidden_features=128,
+        n_gcn_layers=3,
+        n_mbis_hidden_features=128,
+        n_mbis_layers=2,
+        readout_modules="charge",
+        learning_rate=1e-4,
+        gcn_activation=None,
+        readout_activation="Sigmoid",
+    )
+
+    assert model.readout_activation == "Sigmoid"
+    assert isinstance(
+        model.readout_modules["mbis-charges"].readout_layers[1], torch.nn.Sigmoid
+    )
